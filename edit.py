8710ba8343bc619aa5cde1683f654717a5ee23ea
James Tease <jamestease@gmail.com>
2012-02-08 22:19:35 +0000

    Added cookie read/write and basic styling

--- edit.py
+++ edit.py
@@ -2,11 +2,11 @@
 
 number = 0
 
-f = open('index.html', 'w')
+f = open('lines.html', 'w')
 textIn = open('lines.txt', 'r')
 
 for line in textIn:
-    f.write('<input type="checkbox" name="%s" value="true" /> <label for="%s">%s</label><br />\n' % (number, line, line))
+    f.write('<input type="checkbox" name="%s" id="%s" value="true" /> <label for="%s">%s</label><br />\n' % (number, number, number, line))
     number += 1
 
 
